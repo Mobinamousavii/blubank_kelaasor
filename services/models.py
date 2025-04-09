9534a06d83b2c45f5875d23fa05108f220fd213e
Mobinamousavii <mobinamsv6@gmail.com>
2025-04-09 11:34:28 +0330

    login and signuo added

--- services/models.py
+++ services/models.py
@@ -5,15 +5,17 @@ class User(models.Model):
     email = models.EmailField(null=True , blank=True)
     phone = models.CharField(max_length=20)
     national_id = models.CharField(max_length=11, unique=True)
+    password = models.CharField(max_length=20)
 
 class BankAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_bankaccount")
     account_number = models.IntegerField(max_length=20)
     balance = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
 
-class Transfer(models.Model):
+class Transaction(models.Model):
     account = models.ForeignKey(to=BankAccount, on_delete=models.CASCADE, related_name="origin_account")
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     date_time = models.DateTimeField(auto_now_add=True)
+    
 
 
